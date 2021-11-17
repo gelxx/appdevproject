d79e5bdec02c82c82d26c169662bb3b7a3cb16b6
stambalerdy <jkpsoria@gmail.com>
2021-11-18 06:24:57 +0800

    Able to insert notes direct from database

--- cyberinscriber/forms.py
+++ cyberinscriber/forms.py
@@ -2,8 +2,16 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User 
 from django import forms
+from .models import *
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
 		model= User
 		fields = ('username','email','password2','password2')
+
+
+class NotesForm(forms.ModelForm):
+	class Meta:
+		model = Notes
+		fields = '__all__'
+
